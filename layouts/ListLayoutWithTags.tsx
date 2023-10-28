41dcd605309c882c830df355819f74aab03fa50b
Jen Chan <6406037+usrrname@users.noreply.github.com>
2023-10-28 09:57:40 -0400

    adds google analytics tracking id

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -79,9 +79,7 @@ export default function ListLayoutWithTags({
     <>
       <div>
         <div className="pb-6 pt-6">
-          <h1
-            className="font-headings text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
-          >
+          <h1 className="font-headings text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:hidden sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
         </div>
@@ -140,7 +138,8 @@ export default function ListLayoutWithTags({
                             className={`${shantell_sans.variable} text-2xl font-bold leading-8 tracking-tight`}
                             style={{
                               fontVariationSettings: `"BNCE" 2, ""INFM" 30`,
-                            }}>
+                            }}
+                          >
                             <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
                               {title}
                             </Link>
