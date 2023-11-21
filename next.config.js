cd39b68bea048452e5b086130a66b1f2558eb953
Timothy Lin <timothy0336@hotmail.com>
2023-11-21 10:00:16 +0800

    upgrade to next 14.0.3 and pliny 0.0.6

--- next.config.js
+++ next.config.js
@@ -67,7 +67,12 @@ module.exports = () => {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
     images: {
-      domains: ['picsum.photos'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'picsum.photos',
+        },
+      ],
     },
     async headers() {
       return [
