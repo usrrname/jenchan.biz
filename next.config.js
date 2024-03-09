f454330b1692fe1f1dd4d50108d5080119af16d5
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-03-09 10:47:19 -0500

    lint: eslint fixes

--- next.config.js
+++ next.config.js
@@ -58,7 +58,6 @@ const securityHeaders = [
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 module.exports = () => {
-
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
     reactStrictMode: true,
