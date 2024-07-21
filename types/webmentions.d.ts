558e61fcceaf1ab43ba436f20a99a3f8c0011d3d
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-07-21 11:23:13 -0400

    lint: adds types for webmention

---
+++ types/webmentions.d.ts
@@ -0,0 +1,44 @@
+type WebMentionPostResponse = {
+  links: WebMentionPost[]
+}
+
+type WebMentionPost = {
+  data: WebMentionPostData
+  activity: {
+    type: 'like' | 'repost' | 'mention' | 'reply'
+  }
+  source: URL | string
+  verified: boolean
+  verified_date: string
+  id: number
+  private: boolean
+  target: URL
+}
+
+type WebMentionPostData = {
+  author: WebMentionCommentAuthor
+  name: string | null
+  url: URL
+  content: string | unknown | null
+  published?: Date | number | null
+  published_ts?: number | null
+}
+
+type WebMentionCommentAuthor = {
+  name: string
+  photo: string
+  url: URL
+}
+
+interface WebMentionReplies {
+  author: WebMentionCommentAuthor
+  content?: string
+  url: URL
+  published?: WebMentionPostData['published']
+  published_ts?: WebMentionPostData['published_ts']
+}
+
+interface WebMentionReaction {
+  author: WebMentionCommentAuthor
+  url: URL
+}
