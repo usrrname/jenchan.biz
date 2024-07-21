eac4664b1e16b734a5c523320a2ed56707daced2
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-07-21 12:03:33 -0400

    config: allow display of avatars from webmention.io

--- next.config.js
+++ next.config.js
@@ -74,6 +74,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+         {
+          protocol: 'https',
+          hostname: 'webmention.io',
+        },
       ],
     },
     async headers() {
