f3b6428e06e21b3aa68a2cffa85f85a265408edb
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-07-21 16:57:11 -0400

    feat: adds server side functions for getting published posts on devTO

---
+++ app/api/devToPosts.ts
@@ -0,0 +1,14 @@
+const getDevToPublishedArticles = async () => {
+  'use server'
+  const endpoint = `https://dev.to/api/articles/me/published`
+  const headers = new Headers()
+  headers.append('api-key', `${process.env.NEXT_DEVTO_API_KEY}`)
+  headers.append('accept', 'application/vnd.forem.api-v1+json')
+
+  const res = await fetch(endpoint, {
+      headers: headers,
+      cache: 'force-cache'
+  })
+  return res.json()
+}
+export default getDevToPublishedArticles
---
+++ app/api/getArticleById.ts
@@ -0,0 +1,15 @@
+const getArticleById = async (id: number) => {
+    'use server'
+    const endpoint = `https://dev.to/api/articles/${id}`
+    
+    const headers = new Headers()
+    headers.append('api-key', `${process.env.NEXT_DEVTO_API_KEY}`)
+    headers.append('accept', 'application/vnd.forem.api-v1+json')
+  
+    const res = await fetch(endpoint, {
+        headers: headers,
+        cache: 'force-cache'
+    })
+    return res.json()
+}
+export default getArticleById
