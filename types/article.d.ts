d66cabd2e2cfb162c7f1f996e51aa6e1259659e8
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-07-21 20:21:28 -0400

    lint: adds types for DevToArticle

---
+++ types/article.d.ts
@@ -0,0 +1,8 @@
+type DevToArticleStats = {
+    id: number;
+    path: string;
+    url: string;
+    positive_reaction_count: number;
+    public_reaction_count: number;
+    comments_count: number;
+  };
