e25aa09e7f8622ef95d8d6a9ce9b46c92588d5d6
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-10-26 11:24:33 -0400

    girl made her own agile glossary

---
+++ app/glossary/page.tsx
@@ -0,0 +1,33 @@
+import GlossaryItem from '@/components/GlossaryItem'
+import { genPageMetadata } from 'app/seo'
+import { allGlossaryDefinitions } from 'contentlayer/generated'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+const POSTS_PER_PAGE = 25
+
+export const metadata = genPageMetadata({ title: 'Glossary' })
+
+export default function Page() {
+    const definitions = allCoreContent(sortPosts(allGlossaryDefinitions))
+    const pageNumber = 1
+
+    const pagination = {
+        currentPage: pageNumber,
+        totalPages: Math.ceil(definitions.length / POSTS_PER_PAGE),
+    }
+
+    const sortedDefinitions = allGlossaryDefinitions.sort((a, b) => a.title.localeCompare(b.title))
+    return (
+        <div>
+            <p className='my-6'>In my time across many dysfunctional setups, I've come up with the following terms to humor myself when things are inefficient or hard to figure out.</p>
+
+            {sortedDefinitions.map((definition, index) => (
+                <div className="border-l-gray-200" key={index}>
+                    <GlossaryItem GlossaryItem content={definition} key={definition.path} >
+                        {definition.summary}
+                    </GlossaryItem>
+                </div>
+            ))
+            }
+        </div >
+    )
+}
