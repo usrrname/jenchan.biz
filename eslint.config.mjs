e25ac742a47e068107710a74aadd2a1f9cba99c9
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-12-13 11:13:18 -0500

    eslint: warn instead of err

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
+import globals from 'globals'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -50,7 +50,7 @@ export default [
     rules: {
       'prettier/prettier': 'error',
       'react/react-in-jsx-scope': 'off',
-
+      'prefer-const': 'warn',
       'jsx-a11y/anchor-is-valid': [
         'error',
         {
