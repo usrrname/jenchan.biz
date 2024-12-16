04bf40239dd3385afe05c59afde79dc066af60ac
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-12-15 21:04:47 -0500

    chore: formatting

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -29,15 +29,11 @@ const root = process.cwd()
 const isProduction = process.env.NODE_ENV === 'production'
 
 // heroicon mini link
-const icon = fromHtmlIsomorphic(
-  `
-  <span class="content-header-link">
+const icon = fromHtmlIsomorphic(`<span class="content-header-link">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 linkicon">
   <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
   <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
-  </svg>
-  </span>
-`,
+  </svg></span>`,
   { fragment: true }
 )
 
@@ -103,7 +99,6 @@ export const GlossaryDefinition = defineDocumentType(() => ({
     draft: { type: 'boolean' },
     summary: { type: 'string' },
     images: { type: 'json' },
-    authors: { type: 'list', of: { type: 'string' } },
     layout: { type: 'string' },
     canonicalUrl: { type: 'string' },
   },
@@ -215,6 +210,6 @@ export default makeSource({
     const { allBlogs, allGlossaryDefinitions } = await importData()
     createTagCount(allBlogs)
     createSearchIndex(allBlogs)
-    createTagCount(allGlossaryDefinitions)
+    createSearchIndex(allGlossaryDefinitions)
   },
 })
