96975bc96f5e117f5623386dc1ac380d6e2d1251
Jen Chan <6406037+usrrname@users.noreply.github.com>
2024-12-15 21:04:24 -0500

    style: adjust content-width

--- components/GlossaryItem.tsx
+++ components/GlossaryItem.tsx
@@ -18,13 +18,13 @@ export default function GlossaryItem({
 
     return (
         <article>
-            <div className="-pl-4 space-y-1 border-l border-solid dark:border-l-gray-400">
+            <div className="-pl-4 space-y-1 ">
 
                 <details className="prose max-w-none py-4 dark:prose-invert first:pt-0 last:pb-0 hover:cursor-pointer">
 
                     <summary className="text-md text-gray-600 dark:text-gray-400 text-decoration-none z-1">{title}</summary>
 
-                    <div className="text-sm mt-2 pb-5 pl-5 md:max-w-[768px] lg:max-w-[900px]"
+                    <div className="mt-2 pb-5 pl-5 md:max-w-[768px] lg:max-w-[900px] leading-6"
                         dangerouslySetInnerHTML={{ __html: body.raw }}
                     />
                 </details>
