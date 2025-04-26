06023e2230da46ea50009e822324c21ceb4c8501
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-04-26 01:54:04 -0400

    feat: enhance LinkCard component with optional author prop and update blog contentfeat: update blog

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 interface LinkCardProps {
+    author?: string;
     title: string;
     image: string;
     description: string;
@@ -9,6 +10,7 @@ interface LinkCardProps {
 }
 
 const LinkCard: React.FC<LinkCardProps> = ({
+    author,
     title,
     image,
     description,
@@ -16,13 +18,16 @@ const LinkCard: React.FC<LinkCardProps> = ({
     url,
 }) => {
     return (
-        <a href={url} target="_blank" rel="noopener noreferrer" className="inline-block rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-200 ease-in-out text-decoration-none">
+        <a href={url} target="_blank" rel="noopener noreferrer" className="inline-block rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-200 ease-in-out !text-decoration-none">
             <img src={image} alt={`OpenGraph image for ${title}`} className="w-full h-48 object-cover !m-0" loading="lazy" />
             <div className="p-4">
-                <p className="text-xl font-semibold mb-2">{title}</p>
+                <p className="text-lg font-semibold">{title}</p>
+                {author && (
+                    <p className="text-gray-600 text-sm mb-2">{author}</p>
+                )}
                 <p className="text-gray-600 text-sm mb-2">{description}</p>
                 {date && (
-                    <p className="text-gray-500 text-xs">Published: {date}</p>
+                    <p className="text-gray-500 text-xs">{date}</p>
                 )}
             </div>
         </a>
