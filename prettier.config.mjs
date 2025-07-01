c8679275f7f3ed49a8e64749020538867cab10d3
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-07-01 12:41:45 -0400

    chore: update prettier configuration to exclude additional directories

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -6,6 +6,15 @@ const config = {
   trailingComma: 'es5',
   bracketSpacing: true,
   plugins: ['prettier-plugin-tailwindcss'],
+  exclude: [
+    'node_modules',
+    'dist',
+    '.next',
+    'build',
+    'public',
+    'wrangler.toml',
+    'wrangler.jsonc',
+  ],
 }
 
 export default config
