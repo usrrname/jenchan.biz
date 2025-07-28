3b664427740029be4079242967240c2716e433ce
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-07-28 09:31:05 -0400

    chore: update caching strategy in open-next.config.ts to use static assets incremental cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,6 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
-import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import stableIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache'
 
 export default defineCloudflareConfig({
-  incrementalCache: r2IncrementalCache
+  incrementalCache: stableIncrementalCache
 })
