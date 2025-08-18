50c2823530dd3a646f5e987534003a896b525502
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-08-18 19:33:15 -0400

    refactor: update incremental cache configuration to use regional cache with R2 support

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,10 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
-import stableIncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache'
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache'
+import { withRegionalCache } from '@opennextjs/cloudflare/overrides/incremental-cache/regional-cache'
 
 export default defineCloudflareConfig({
-  incrementalCache: stableIncrementalCache,
-  enableCacheInterception: true
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: 'long-lived',
+    shouldLazilyUpdateOnCacheHit: false
+  })
 })
