55a8677a9afed7e31d831fc0d48ae9607ad74bc9
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-08-18 17:34:41 -0400

    fix: replace img with Image component for optimized loading in LinkCard

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -1,36 +1,45 @@
-import React from 'react';
+import Image from 'next/image'
+import React from 'react'
 interface LinkCardProps {
-    author?: string;
-    title: string;
-    image: string;
-    description: string;
-    date?: string;
-    url: string;
+  author?: string
+  title: string
+  image: string
+  description: string
+  date?: string
+  url: string
 }
 
 const LinkCard: React.FC<LinkCardProps> = ({
-    author,
-    title,
-    image,
-    description,
-    date,
-    url,
+  author,
+  title,
+  image,
+  description,
+  date,
+  url
 }) => {
-    return (
-        <a href={url} target="_blank" rel="noopener noreferrer" className="inline-block rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-200 ease-in-out !text-decoration-none">
-            {image && <img src={image} className="w-full h-48 object-cover !m-0" loading="lazy" alt={`${title}`} />}
-            <div className="p-4">
-                <p className="text-lg font-semibold">{title}</p>
-                {author && (
-                    <p className="text-gray-600 text-sm mb-2">{author}</p>
-                )}
-                <p className="text-gray-600 text-sm mb-2">{description}</p>
-                {date && (
-                    <p className="text-gray-500 text-xs">{date}</p>
-                )}
-            </div>
-        </a>
-    );
-};
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="!text-decoration-none inline-block overflow-hidden rounded-lg shadow-md transition-shadow duration-200 ease-in-out hover:shadow-lg"
+    >
+      {image && (
+        <Image
+          src={image}
+          className="!m-0 h-48 w-full object-cover"
+          loading="lazy"
+          alt={`${title}`}
+        />
+      )}
+      <div className="p-4">
+        <p className="text-lg font-semibold">{title}</p>
+        {author && <p className="mb-2 text-sm text-gray-600">{author}</p>}
+        <p className="mb-2 text-sm text-gray-600">{description}</p>
+        {date && <p className="text-xs text-gray-500">{date}</p>}
+      </div>
+    </a>
+  )
+}
 
-export default LinkCard; 
+export default LinkCard
