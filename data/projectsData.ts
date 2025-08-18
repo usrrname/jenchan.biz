605d4681745d58c92b7eaec77e726ed55018aaaa
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-08-18 12:39:01 -0400

    feat: add project entry

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,6 +11,13 @@ interface Project {
 }
 
 const projectsData: Project[] = [
+  // {
+  //   title: 'Various features: Sports Betting Platform',
+  //   description: `Expanded front end capability to launch from 20 to 400+, contributing to 50% of 2024 revenue at Rivalry`,
+  //   company: `Rivalry`,
+  //   imgSrc: '/static/images/work/rivalry-casinov2.jpg',
+  //   href: 'https://rivalry.com/casino',
+  // },
   {
     title: 'Enterprise Design System',
     description: `Led and support 12-30 contributors to build and maintain a design system with web components for use in Angular 1-16 and React 16-18 projects. Components are now used in over 150+ products`,
@@ -21,33 +28,33 @@ const projectsData: Project[] = [
       links: [
         {
           title: `Co-Counsel`,
-          href: 'https://legal.thomsonreuters.com/en/c/cocounsel/generative-ai-suite',
+          href: 'https://legal.thomsonreuters.com/en/c/cocounsel/generative-ai-suite'
         },
         {
           title: 'Onvio',
-          href: 'https://tax.thomsonreuters.com/en/accounting-solutions/onvio',
+          href: 'https://tax.thomsonreuters.com/en/accounting-solutions/onvio'
         },
         {
           title: `Practical Law`,
-          href: `https://thomsonreuters.hubs.vidyard.com/watch/47WU7AiPsTfpao2wQqBQdN?autoplay=2&second=12.27`,
+          href: `https://thomsonreuters.hubs.vidyard.com/watch/47WU7AiPsTfpao2wQqBQdN?autoplay=2&second=12.27`
         },
         {
           title: `Westlaw Edge`,
-          href: 'https://www.thomsonreuters.ca/en/products/westlaw-edge',
+          href: 'https://www.thomsonreuters.ca/en/products/westlaw-edge'
         },
         {
           title: `Developer Portal`,
-          href: 'https://developers.thomsonreuters.com/',
-        },
-      ],
-    },
+          href: 'https://developers.thomsonreuters.com/'
+        }
+      ]
+    }
   },
   {
     title: 'Radius 2.0 design system accelerator',
     company: 'Rangle.io',
     description: `Worked on a rotating team of 4 to develop a command line tool with a boilerplate Figma UI Kit to accelerate the bootstrapping of design systems based on selection of JS framework, CSS strategy, and CI tool`,
     imgSrc: '/static/images/work/radius-home.svg',
-    href: 'https://radius.rangle.io/',
+    href: 'https://radius.rangle.io/'
   },
   {
     title: 'Idea to MVP: Insurance Payer Health Portal',
@@ -59,28 +66,28 @@ const projectsData: Project[] = [
       links: [
         {
           title: 'AppSphere',
-          href: `https://www.smiledigitalhealth.com/appsphere`,
+          href: `https://www.smiledigitalhealth.com/appsphere`
         },
         {
           title: 'Insurance Payer Member Portal',
-          href: 'https://www.smiledigitalhealth.com/payer-member-portal',
-        },
-      ],
-    },
+          href: 'https://www.smiledigitalhealth.com/payer-member-portal'
+        }
+      ]
+    }
   },
   {
     title: 'Modernizing heart procedure wait time system',
     company: 'Smile CDR',
     description: `Revamping a system that hospital administrators use to calculate heart disease and stroke procedure priority in Ontario. Heroic feats involve retrofitting a typeahead to also become a dropdown selector, and implementing 250 combinations of live validations on form fields in Angular 9`,
     imgSrc: '/static/images/work/wtis.png',
-    href: 'https://youtu.be/0dRKoS6YudU?si=h3hb515Nt-xJ1awE&t=149',
+    href: 'https://youtu.be/0dRKoS6YudU?si=h3hb515Nt-xJ1awE&t=149'
   },
   {
     title: 'User account settings and portfolio allocation features',
     company: `Capintel`,
     description: `Internal user account settings and portfolio allocation features for a product connecting asset managers and financial advisors. Designs by Niclas Erst at Fintory`,
     imgSrc: '/static/images/work/allocation.png',
-    href: 'https://www.capintel.com/independent-advisors#:~:text=Instant%20analytics%20at%20your%20fingertips.',
+    href: 'https://www.capintel.com/independent-advisors#:~:text=Instant%20analytics%20at%20your%20fingertips.'
   },
   {
     title: 'V/Art: A virtual art gallery',
@@ -93,11 +100,11 @@ const projectsData: Project[] = [
       links: [
         {
           title: '"Venturing into the Virtual World", Globe and Mail, 2017.',
-          href: 'https://www.theglobeandmail.com/arts/art-and-architecture/venturing-into-the-virtual-world-with-a-new-artapp/article37066961/',
-        },
-      ],
-    },
-  },
+          href: 'https://www.theglobeandmail.com/arts/art-and-architecture/venturing-into-the-virtual-world-with-a-new-artapp/article37066961/'
+        }
+      ]
+    }
+  }
 ]
 
 export default projectsData
