58c97a4e88855e82f358c8d896a2aeed6355dd1f
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-09-14 13:18:24 -0400

    refactor: remove 'Tags' link from header navigation for streamlined menu

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,11 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
   { href: '/work', title: 'Work' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/JenChanResume.pdf', title: 'Resume' },
+  { href: '/JenChanResume.pdf', title: 'Resume' }
 ]
 
 export default headerNavLinks
