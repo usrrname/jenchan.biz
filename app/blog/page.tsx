062ce05cdfbd42bbc0191e80b05fe2623015527c
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-09-27 13:10:32 -0400

    fix: update tag counts in tag-data.json and adjust initial page number in BlogPage component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,7 +9,7 @@ export const metadata = genPageMetadata({ title: 'Blog' })
 
 export default async function BlogPage(props: { searchParams: Promise<{ page: string }> }) {
   const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
+  const pageNumber = 2
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
   const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE * pageNumber)
   const pagination = {
